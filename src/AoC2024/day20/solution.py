fe3e25f5d50cb135e420bf463528d5e642b07580
burnysc2 <gamingburny@gmail.com>
2026-01-02 06:53:06 +0100

    Refactor for part 2

--- src/AoC2024/day20/solution.py
+++ src/AoC2024/day20/solution.py
@@ -23,7 +23,9 @@ def add_vec(v1: Vec, v2: Vec) -> Vec:
     return v1[0] + v2[0], v1[1] + v2[1]
 
 
-def solve(input_text: str) -> tuple[int, int]:
+def solve(
+    input_text: str, cheat_duration: int = 2, min_time_saved: int = 100
+) -> tuple[int, int]:
     grid = input_text.splitlines()
     neighbors: list[Vec] = [(0, 1), (0, -1), (-1, 0), (1, 0)]
     start = next(
@@ -76,16 +78,18 @@ def solve(input_text: str) -> tuple[int, int]:
         pos = (pos_x, pos_y)
 
         char = get_char(grid, pos)
-        # Cheat timed out
-        if cheat and char == "#" and cheat_active <= 0:
-            return
 
+        # Already used this path
+        if cheat and cheat_used_at_pos != (-1, -1) and pos in came_from_score:
+            total_distance = came_from_score[pos] + distance_to_start
+            if path_length_without_cheat < total_distance:
+                return
+            if cheat_active <= 0:
+                paths.append((total_distance, cheat_used_at_pos))
+                return
         # End reached
         if not cheat and char == end_char:
             path_length_without_cheat = distance_to_start
-            return
-        if cheat and cheat_used_at_pos != (-1, -1) and pos in came_from_score:
-            paths.append((came_from_score[pos] + distance_to_start, cheat_used_at_pos))
             return
         if cheat and char == end_char:
             paths.append((distance_to_start, cheat_used_at_pos))
@@ -95,10 +99,6 @@ def solve(input_text: str) -> tuple[int, int]:
             already_visited.add(pos)
         else:
             already_visited_cheat.add((pos, cheat_used_at_pos))
-
-        # Already found a path without cheat, stop all attempt with a longer path
-        if cheat and path_length_without_cheat < distance_to_start:
-            return
 
         for neighbor in neighbors:
             new_pos = add_vec(pos, neighbor)
@@ -112,9 +112,9 @@ def solve(input_text: str) -> tuple[int, int]:
             if char in {".", end_char}:
                 if not cheat:
                     heappush(queue, (distance_to_start + 1, *new_pos))
+                    # Cache for cheat path: already seen this position, we know distance to end
                     if distance_to_start + 1 < came_from_score.get(new_pos, math.inf):
                         came_from_score[new_pos] = distance_to_start + 1
-                        # came_from[new_pos] = pos
                 else:
                     heappush(
                         queue_cheat,
@@ -125,12 +125,22 @@ def solve(input_text: str) -> tuple[int, int]:
                             cheat_used_at_pos,
                         ),
                     )
-            # TODO Allow walk through wall if 1 <= cheat_active?
+            # Go through wall
+            if cheat and 1 < cheat_active and char == "#":
+                heappush(
+                    queue_cheat,
+                    (
+                        distance_to_start + 1,
+                        *new_pos,
+                        cheat_active - 1,
+                        cheat_used_at_pos,
+                    ),
+                )
             # Activate cheat
             if cheat and cheat_used_at_pos == (-1, -1) and char == "#":
                 heappush(
                     queue_cheat,
-                    (distance_to_start + 1, *new_pos, 1, pos),
+                    (distance_to_start + 1, *new_pos, cheat_duration - 1, pos),
                 )
 
     # Build path from end to finish, for came_from_score
@@ -142,15 +152,15 @@ def solve(input_text: str) -> tuple[int, int]:
     while queue_cheat:
         iterate(cheat=True)
 
-    paths_that_save_100ps = sorted(
+    path_that_save_time = sorted(
         [
             i
             for i in paths
-            if i[0] <= path_length_without_cheat - 100 and i[1] is not None
+            if i[0] <= path_length_without_cheat - min_time_saved and i[1] is not None
         ]
     )
 
-    answer_part1 = len(paths_that_save_100ps)
+    answer_part1 = len(path_that_save_time)
 
     answer_part2 = 0
     return answer_part1, answer_part2
@@ -158,20 +168,22 @@ def solve(input_text: str) -> tuple[int, int]:
 
 def main():
     # Part 1
-    solution_example = solve(input_example_text)
+    solution_example = solve(input_example_text, cheat_duration=2, min_time_saved=100)
     answer_example_part1 = solution_example[0]
     print(f"The solution for the example for part1 is: {answer_example_part1=}")
     assert answer_example_part1 == 0
 
-    solution = solve(input_text)
-    answer_part1 = solution[0]
+    solution = solve(input_text, cheat_duration=2, min_time_saved=100)
+    answer_part1 = solution[0]  # 1381
     print(f"The solution for the part1 is: {answer_part1=}")
 
     # Part 2
+    solution_example = solve(input_example_text, cheat_duration=20, min_time_saved=50)
     answer_example_part2 = solution_example[1]
     print(f"The solution for the example for part2 is: {answer_example_part2=}")
-    assert answer_example_part2 == 11387
+    assert answer_example_part2 == 0
 
+    solution = solve(input_text, cheat_duration=20, min_time_saved=50)
     answer_part2 = solution[1]
     print(f"The solution for the part2 is: {answer_part2=}")
 
